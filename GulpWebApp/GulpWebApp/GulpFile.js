2f9b38099a21c1d365d31eafc7d9fcbe107e6bb3
mlockett <mike@DADQUAD>
2016-06-25 22:33:48 -0600

    Clean-up 1

--- GulpWebApp/GulpWebApp/GulpFile.js
+++ GulpWebApp/GulpWebApp/GulpFile.js
@@ -1,1 +1,34 @@
-﻿var gulp = require("gulp");
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var yargs = require("yargs").argv;
+var sass = require("gulp-sass");
+var htmlReplace = require("gulp-html-replace");
+
+gulp.task("default", function () {
+    console.log("Hello... you hit the default task");
+});
+
+gulp.task("deploy", ["compile-sass"],
+   function () {
+       // get the deployment temp folder (passed in from pubxml file(s) process)
+       var deployTempFolder = yargs.folder;
+
+       console.log("**************************************");
+       console.log("Using temp deploy folder: " + deployTempFolder);       
+
+       gulp.src(deployTempFolder + "aaa.html")
+          .pipe(htmlReplace({
+              "css": "styles1.min.css"
+          }))
+          .pipe(gulp.dest(deployTempFolder));
+
+   });
+
+gulp.task("compile-sass", function () {
+    console.log("Compiling SASS")
+});
