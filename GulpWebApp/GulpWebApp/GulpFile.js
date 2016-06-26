b463f93dbd78a8b1e277bbb91f1219075ff2f21b
mlockett <mike@DADQUAD>
2016-06-25 23:40:57 -0600

    Remove Insight stuff

--- GulpWebApp/GulpWebApp/GulpFile.js
+++ GulpWebApp/GulpWebApp/GulpFile.js
@@ -1,9 +1,4 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
-var gulp = require("gulp");
+﻿var gulp = require("gulp");
 var concat = require("gulp-concat");
 var yargs = require("yargs").argv;
 var sass = require("gulp-sass");
@@ -19,13 +14,19 @@ gulp.task("deploy", ["compile-sass"],
        var deployTempFolder = yargs.folder;
 
        console.log("**************************************");
-       console.log("Using temp deploy folder: " + deployTempFolder);       
+       console.log("Using temp deploy folder: " + deployTempFolder);
 
        gulp.src(deployTempFolder + "aaa.html")
           .pipe(htmlReplace({
-              "css": "styles1.min.css"
+              "css": "styles.all.min.css"
           }))
           .pipe(gulp.dest(deployTempFolder));
+
+       gulp.src(deployTempFolder + "Views/Home/*.cshtml")
+          .pipe(htmlReplace({
+              "css": "styles.all.min.css"
+          }))
+          .pipe(gulp.dest(deployTempFolder + "Views/Home/"));
 
    });
 
